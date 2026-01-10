ae6cad5c10b9c777c3195ff60abdad4a97b6b2ef
Stephen Rollins <rollin23@byui.edu>
2026-01-09 22:58:40 -0700

    js-changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,22 @@
 import { getParkData } from "./parkService.mjs";
+
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
+
+document.title = parkData.fullName;
+
+const heroImage = document.querySelector(".hero-banner Img");
+heroImage.src = parkData.images[0].url;
+heroImage.alt = parkData.images[0].altText;
+
