7744a188f23415a0cbd98d8dc942cfd647dd9063
Stephen Rollins <rollin23@byui.edu>
2026-02-07 21:15:43 -0700

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
-
 import "../css/style.css";
 import "../css/home.css";
 
